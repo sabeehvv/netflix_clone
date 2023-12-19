cbe5f8fe15f7a60934962123016585475a9dab0d
Mohamed Sabeeh <sabeehvv@gmail.com>
2023-12-19 16:57:48 +0530

    Updated

---
+++ src/Components/Banner/Banner.js
@@ -0,0 +1,37 @@
+import React, { useEffect, useState } from 'react'
+import axios from '../../axios'
+import {API_KEY,imageUrl} from '../../constants/constants'
+import './Banner.css'
+
+function Banner() {
+  const [movie,setMovie] = useState()
+  useEffect(()=>{
+    axios.get(`trending/all/week?api_key=${API_KEY}&language=en-US`).then((response)=>{
+      console.log(response.data.results[0])
+      const results = response.data.results
+      const randomIndex = Math.floor(Math.random() * results.length);
+      setMovie(response.data.results[randomIndex])
+    })
+  },[])
+  return (
+    <div className='banner' style={{backgroundImage:`url(${movie? imageUrl+movie.backdrop_path:''})`}}>
+      <div className='content'>
+        <h1 className='title'>{movie?movie.title:''}</h1>
+        <div className='banner_buttons'>
+          <button className='button'>
+            play
+          </button>
+          <button className='button'>
+            My list
+          </button>
+        </div>
+        <h1 className='description'>{movie?movie.overview:''}</h1>
+      </div>
+      <div className="fade_bottom">
+
+      </div>
+    </div>
+  )
+}
+
+export default Banner
